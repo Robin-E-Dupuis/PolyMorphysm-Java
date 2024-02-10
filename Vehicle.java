65381fdf7770a8c8bec80e6f4bf1a7bf9c1a4f93
Robin-E-Dupuis <155603918+Robin-E-Dupuis@users.noreply.github.com>
2024-02-10 09:08:00 -0600

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,11 @@
+//author robin dupuis
+public class Vehicle {
+    
+    
+    public void Go(){
+        System.out.println("Your vehicle is weird");
+    }
+    public void Stop(){
+        System.out.println("Your vehicle is weird");
+    }
+}
